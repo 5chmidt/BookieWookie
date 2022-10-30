07706b8f528bee6e35eab269eca6bd1fa799b88b
5chmidt <pschmidt@buffalo.edu>
2022-10-29 22:33:51 -0400

    Added context entity.

---
+++ BookieWookie/BookieWookie.API/Entities/WookieBookieContext.cs
@@ -0,0 +1,20 @@
+﻿
+namespace WebAPI.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using System.Collections.Generic;
+
+    public class DbContextClass : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        public DbContextClass(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+
+    }
+}
