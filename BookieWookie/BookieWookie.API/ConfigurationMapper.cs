1b1e8e620640567b1d8da020066242f6332d6230
5chmidt <pschmidt@buffalo.edu>
2022-10-30 13:42:37 -0400

    Added settings per build type.

--- BookieWookie/BookieWookie.API/ConfigurationMapper.cs
+++ BookieWookie/BookieWookie.API/ConfigurationMapper.cs
@@ -8,6 +8,11 @@
             AppSetting = new ConfigurationBuilder()
                     .SetBasePath(Directory.GetCurrentDirectory())
                     .AddJsonFile("appsettings.json")
+#if DEBUG
+                    .AddJsonFile($"appsettings.Development.json")   //TODO: Add Staging settings.
+#else
+                    .AddJsonFile($"appsettings.Production.json")
+#endif
                     .Build();
         }
     }
