68c0bf7d2c41c6ff0847fccada7ee9485e5f2e6e
5chmidt <pschmidt@buffalo.edu>
2022-10-29 22:34:15 -0400

    Renamed login model

---
+++ BookieWookie/BookieWookie.API/Models/AuthenticationRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace BookieWookie.API.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class AuthenticationRequest
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
