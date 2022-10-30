c5e69be4b6a7634ed310d060cc0261f19bcdbbe1
5chmidt <pschmidt@buffalo.edu>
2022-10-30 14:11:53 -0400

    Added authorization filter.

---
+++ BookieWookie/BookieWookie.API/Helpers/AuthorizeAttribute.cs
@@ -0,0 +1,20 @@
+﻿namespace BookieWookie.API.Helpers
+{
+    using BookieWookie.API.Entities;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class AuthorizeAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            User? user = context.HttpContext.Items["User"] as User;
+            if (user == null)
+            {
+                // not logged in
+                context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
+            }
+        }
+    }
+}
