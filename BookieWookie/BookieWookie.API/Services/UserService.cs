4aa0784dcc4266a6e1b85ddcc3fe0fe98cc747d4
5chmidt <pschmidt@buffalo.edu>
2022-10-30 18:19:56 -0400

    Impliemented services for updating users.

--- BookieWookie/BookieWookie.API/Services/UserService.cs
+++ BookieWookie/BookieWookie.API/Services/UserService.cs
@@ -16,7 +16,8 @@
     public interface IUserService
     {
         JWTTokenResponse Authenticate(AuthenticateRequest model);
-        JWTTokenResponse CreateUser(CreateUserRequest model);
+        JWTTokenResponse CreateUser(UserRequest model);
+        UserRequest UpdateUser(UserRequest model);
         IEnumerable<User> GetAll();
         User GetById(int id);
     }
@@ -31,7 +32,6 @@
             this.Configuration = configuration;
         }
 
-        [HttpPost("authenticate")]
         public JWTTokenResponse Authenticate(AuthenticateRequest model)
         {
             // find user in database //
@@ -54,34 +54,94 @@
                 return null;
             }
 
-            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
-                API.ConfigurationManager.AppSetting["JWT:Secret"]));
-
-            var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
-            var tokeOptions = new JwtSecurityToken(
-                issuer: API.ConfigurationManager.AppSetting["JWT:ValidIssuer"],
-                audience: API.ConfigurationManager.AppSetting["JWT:ValidAudience"],
-                claims: new List<Claim>(),
-                expires: DateTime.Now.AddMinutes(60),
-                signingCredentials: signinCredentials
-            );
-            var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
-            return new JWTTokenResponse { Token = tokenString };
+            Claim[] claims = new Claim[0];
+            return IssueToken(claims);
         }
 
-        public JWTTokenResponse CreateUser(CreateUserRequest model)
+        public JWTTokenResponse CreateUser(UserRequest model)
         {
-            throw new NotImplementedException();
+            var user = new User();
+            using (var db = new WookieBookieContext(this.Configuration))
+            {
+                if (db.Users.Where(u => u.Username == model.Username).Count() > 0)
+                {
+                    throw new AuthenticationException($"Username '{model.Username}' already exists");
+                }
+
+                user.Username = model.Username;
+                var authService = new AuthenticationService();
+                user.FirstName = model.FirstName;
+                user.LastName = model.LastName;
+                user.Pseudonym = model.Pseudonym;
+                user.Salt = authService.CreateSalt();
+                user.Hash = authService.HashPassword(model.Password, user.Salt);
+                db.Users.Add(user);
+                db.SaveChanges();
+            }
+
+            Claim[] claims = new Claim[]
+            {
+                new Claim(nameof(User.Id), user.Id.ToString()),
+                new Claim(nameof(User.Username), user.Username),
+            };
+            return IssueToken(claims);
+        }
+
+        public UserRequest UpdateUser(UserRequest model)
+        {
+            var user = new User();
+            using (var db = new WookieBookieContext(this.Configuration))
+            {
+                user = db.Users.SingleOrDefault(u => u.Username == model.Username);
+                if (user == null)
+                {
+                    throw new AuthenticationException($"Username '{model.Username}' does not exist.");
+                }
+
+                user.Username = model.Username;
+                var authService = new AuthenticationService();
+                user.FirstName = model.FirstName;
+                user.LastName = model.LastName;
+                user.Pseudonym = model.Pseudonym;
+                user.Salt = authService.CreateSalt();
+                user.Hash = authService.HashPassword(model.Password, user.Salt);
+                db.SaveChanges();
+            }
+
+            Claim[] claims = new Claim[0];
+            return IssueToken(claims);
         }
 
         public IEnumerable<User> GetAll()
         {
-            throw new NotImplementedException();
+            using (var db = new WookieBookieContext(this.Configuration))
+            {
+                return db.Users.ToArray();
+            }
         }
 
         public User GetById(int id)
         {
-            throw new NotImplementedException();
+            using (var db = new WookieBookieContext(this.Configuration))
+            {
+                return db.Users.FirstOrDefault(u => u.Id == id);
+            }
+        }
+
+        private JWTTokenResponse IssueToken(Claim[] claims)
+        {
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                API.ConfigurationManager.AppSetting["JWT:Secret"]));
+            var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+            var tokenOptions = new JwtSecurityToken(
+                issuer: API.ConfigurationManager.AppSetting["JWT:ValidIssuer"],
+                audience: API.ConfigurationManager.AppSetting["JWT:ValidAudience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: signinCredentials
+            );
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+            return new JWTTokenResponse { Token = tokenString };
         }
     }
 }
