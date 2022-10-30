0d022b63d6b320ca11e963ed6ec9c6b70673ec4f
5chmidt <pschmidt@buffalo.edu>
2022-10-30 12:50:20 -0400

    Added model for creating user.

---
+++ BookieWookie/BookieWookie.API/Models/CreateUserRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookieWookie.API.Models
+{
+    public class CreateUserRequest
+    {
+        [Required]
+        public string? UserName { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Pseudonym { get; set; }
+    }
+}
