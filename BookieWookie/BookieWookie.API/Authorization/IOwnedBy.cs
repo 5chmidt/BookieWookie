da3eb47393664f093f3e3eebd91b3adba4dc6bba
5chmidt <pschmidt@buffalo.edu>
2022-11-06 20:29:35 -0500

    Added owned by interface, class and authorization method.

---
+++ BookieWookie/BookieWookie.API/Authorization/IOwnedBy.cs
@@ -0,0 +1,8 @@
+﻿namespace BookieWookie.API.Authorization
+{
+    public interface IOwnedBy
+    {
+        string OwnedBy { get; }
+        void SetOwnedBy(string protectKey);
+    }
+}
---
+++ BookieWookie/BookieWookie.API/Authorization/OwnedBy.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookieWookie.API.Authorization
+{
+    public class OwnedByBase : IOwnedBy
+    {
+        [Required] 
+        [MaxLength(40)] //Guid string
+        public string OwnedBy { get; private set; }
+
+        public void SetOwnedBy(string protectKey)
+        {
+            OwnedBy = protectKey;
+        }
+    }
+}
