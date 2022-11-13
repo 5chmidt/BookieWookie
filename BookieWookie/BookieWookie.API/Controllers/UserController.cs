dcf109e146430758b172fafa0b5bba00bc8a1d94
5chmidt <pschmidt@buffalo.edu>
2022-11-13 17:34:23 -0500

    Added comments to the user controller.

--- BookieWookie/BookieWookie.API/Controllers/UserController.cs
+++ BookieWookie/BookieWookie.API/Controllers/UserController.cs
@@ -6,6 +6,9 @@
     using BookieWookie.API.Models;
     using BookieWookie.API.Services;
 
+    /// <summary>
+    /// User controller for login and CRUD operations.
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class UserController : ControllerBase
@@ -14,12 +17,22 @@
 
         private IConfiguration _configuration;
 
+        /// <summary>
+        /// Initialize a user controller. 
+        /// </summary>
+        /// <param name="userService"></param>
+        /// <param name="configuration"></param>
         public UserController(IUserService userService, IConfiguration configuration)
         {
             _userService = userService;
             _configuration = configuration;
         }
 
+        /// <summary>
+        /// Verify username and password then return a JWT with user claims.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
         [HttpPost("authenticate")]
         public IActionResult Authenticate(AuthenticateRequest model)
         {
@@ -32,6 +45,11 @@
             return Ok(response);
         }
 
+        /// <summary>
+        /// Create a new user.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
         [HttpPost("create")]
         public IActionResult Create(UserRequest model)
         {
@@ -49,6 +67,11 @@
             return Ok(responce);
         }
 
+        /// <summary>
+        /// Update user profile information, (cannot update username).
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
         [AuthorizeOwner]
         [HttpPost("update")]
         public IActionResult Update(UserRequest model)
@@ -66,6 +89,11 @@
             return Ok(model);
         }
 
+        /// <summary>
+        /// Delete a user, only an authenticated user can delete their own profile.
+        /// </summary>
+        /// <param name="id">Id of the user to be removed.</param>
+        /// <returns>Object of the user profile that was deleted.</returns>
         [AuthorizeOwner]
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
@@ -83,9 +111,13 @@
             return Ok(model);
         }
 
+        /// <summary>
+        /// Gets a list of all active users.
+        /// </summary>
+        /// <returns>Array of user objects.</returns>
         [AuthorizeOwner]
-        [HttpGet("all")]
-        public IActionResult GetAll()
+        [HttpGet("get")]
+        public IActionResult Get()
         {
             var users = _userService.GetAll();
             return Ok(users);
