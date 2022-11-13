be9e36d3b7ce0ce03729591b0762ccb6ac0fa95f
5chmidt <pschmidt@buffalo.edu>
2022-11-13 12:22:47 -0500

    Updated database with linked keys.

---
+++ BookieWookie/BookieWookie.API/Migrations/20221113040113_foreign_keys.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookieWookie.API.Migrations
+{
+    public partial class foreign_keys : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Users",
+                newName: "UserId");
+
+            migrationBuilder.RenameColumn(
+                name: "AuthorId",
+                table: "Books",
+                newName: "UserId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Books",
+                newName: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_UserId",
+                table: "Books",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Users_UserId",
+                table: "Books",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Users_UserId",
+                table: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_UserId",
+                table: "Books");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Users",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Books",
+                newName: "AuthorId");
+
+            migrationBuilder.RenameColumn(
+                name: "BookId",
+                table: "Books",
+                newName: "Id");
+        }
+    }
+}
