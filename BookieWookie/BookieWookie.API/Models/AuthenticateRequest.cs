d1ecf6809d915347ec6e7c0afc363e8ced274db8
5chmidt <pschmidt@buffalo.edu>
2022-11-15 21:41:18 -0500

    Added comments for models.

--- BookieWookie/BookieWookie.API/Models/AuthenticateRequest.cs
+++ BookieWookie/BookieWookie.API/Models/AuthenticateRequest.cs
@@ -2,11 +2,20 @@
 {
     using System.ComponentModel.DataAnnotations;
 
+    /// <summary>
+    /// Model for user authentication request.
+    /// </summary>
     public class AuthenticateRequest
     {
+        /// <summary>
+        /// Gets or sets the username, which is required.
+        /// </summary>
         [Required]
         public string? Username { get; set; }
 
+        /// <summary>
+        /// Gets or sets the user password which is required.
+        /// </summary>
         [Required]
         public string? Password { get; set; }
     }
--- BookieWookie/BookieWookie.API/Models/CreateBookRequest.cs
+++ BookieWookie/BookieWookie.API/Models/CreateBookRequest.cs
@@ -2,12 +2,20 @@
 
 namespace BookieWookie.API.Models
 {
+    /// <summary>
+    /// Model for an authenticated user to create a new book entry.
+    /// </summary>
     public class CreateBookRequest
     {
+        /// <summary>
+        /// Gets or sets the book title, (required).
+        /// </summary>
         [Required]
         public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the book description (optional).
+        /// </summary>
         public string Description { get; set; }
-
-
     }
 }
