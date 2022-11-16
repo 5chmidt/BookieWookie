37fb95f9a6fca0737539c652ae09e262f09eea62
5chmidt <pschmidt@buffalo.edu>
2022-11-15 22:11:52 -0500

    Added empty file controller and service.

---
+++ BookieWookie/BookieWookie.API/Controllers/FileController.cs
@@ -0,0 +1,41 @@
+﻿using BookieWookie.API.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookieWookie.API.Controllers
+{
+    /// <summary>
+    /// Controller for uploading and linking files.
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class FileController : Controller
+    {
+        private IConfiguration _configuration;
+
+        /// <summary>
+        /// Initialize FileController with dependency injection.
+        /// </summary>
+        /// <param name="configuration"></param>
+        public FileController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        /// <summary>
+        /// Upload an image.
+        /// </summary>
+        /// <param name="file">File sent via HTTP request.</param>
+        /// <returns>File model.</returns>
+        [HttpPost("UploadImage")]
+        public ActionResult UploadImage(IFormFile file)
+        {
+            if (file.ContentType.Split('/').FirstOrDefault() != "image")
+            {
+                return BadRequest("uploaded file is not an image.");
+            }
+
+            // we can put rest of upload logic here.
+            return Ok(file.FileName);
+        }
+    }
+}
---
+++ BookieWookie/BookieWookie.API/Services/FileService.cs
@@ -0,0 +1,24 @@
+﻿namespace BookieWookie.API.Services
+{
+    public interface IFileService
+    {
+        /// <summary>
+        /// Upload an image file and log who/when it was uploaded.
+        /// </summary>
+        /// <param name="file">File sent via HTTP request.</param>
+        /// <param name="userId">Id of the currently authenticated user.</param>
+        /// <returns>Model of the uploaded file.</returns>
+        Entities.File UploadImage(IFormFile file, int userId);
+
+    }
+
+    /// <inheritdoc/>
+    public class FileService : IFileService
+    {
+        /// <inheritdoc/>
+        public Entities.File UploadImage(IFormFile file, int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
