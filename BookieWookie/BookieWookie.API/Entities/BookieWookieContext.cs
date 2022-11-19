37795789afe195d901754a0993fcacef93972a68
5chmidt <pschmidt@buffalo.edu>
2022-11-19 17:03:01 -0500

    Added comments to dbcontext class.

--- BookieWookie/BookieWookie.API/Entities/BookieWookieContext.cs
+++ BookieWookie/BookieWookie.API/Entities/BookieWookieContext.cs
@@ -5,22 +5,44 @@ namespace BookieWookie.API.Entities
     using Microsoft.Extensions.Options;
     using System.Collections.Generic;
 
+    /// <summary>
+    /// Context for entity framework bookie wookie database.
+    /// </summary>
     public class BookieWookieContext : DbContext
     {
-        protected readonly IConfiguration Configuration;
+        private readonly IConfiguration _configuration;
 
+        /// <summary>
+        /// Initialize db context with dependency injection.
+        /// </summary>
+        /// <param name="configuration"></param>
         public BookieWookieContext(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
 
+        /// <summary>
+        /// DbSet for user model. <see cref="User"/>
+        /// </summary>
         public virtual DbSet<User> Users { get; set; }
+
+        /// <summary>
+        /// Dbset for book model <see cref="Book"/>
+        /// </summary>
         public virtual DbSet<Book> Books { get; set; }
+
+        /// <summary>
+        /// Dbset for file model. <see cref="File"/>
+        /// </summary>
         public virtual DbSet<File> Files { get; set; }
 
+        /// <summary>
+        /// Override with db context connection string from configuration.
+        /// </summary>
+        /// <param name="options"><see cref="DbContextOptions"/></param>
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
-            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
         }
 
     }
