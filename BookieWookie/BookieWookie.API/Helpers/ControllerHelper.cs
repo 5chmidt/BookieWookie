acf50769965c281fcca47d04a3941e696a4d56e0
5chmidt <pschmidt@buffalo.edu>
2022-11-19 11:14:36 -0500

    Added helper to get permissions from current context.

--- BookieWookie/BookieWookie.API/Helpers/ControllerHelper.cs
+++ BookieWookie/BookieWookie.API/Helpers/ControllerHelper.cs
@@ -1,5 +1,6 @@
 ﻿namespace BookieWookie.API.Helpers
 {
+    using BookieWookie.API.Authorization;
     using BookieWookie.API.Entities;
     using Microsoft.AspNetCore.Mvc;
 
@@ -10,10 +11,21 @@
         /// </summary>
         /// <param name="controller">The controller being used currently.</param>
         /// <returns>Int - User ID.</returns>
-        public static int ParseUserIdFromContext(this Controller controller)
+        public static int ParseUserIdFromContext(this ControllerBase controller)
         {
             var user = (User?)controller.HttpContext.Items[nameof(User)];
             return user == null ? 0 : Convert.ToInt32(user.UserId);
+        }
+
+        /// <summary>
+        /// Helper method to parse the current user's permissions.
+        /// </summary>
+        /// <param name="controller"><see cref="ControllerBase"/></param>
+        /// <returns><see cref="PermissionLevel"/></returns>
+        public static PermissionLevel ParsePermissionFromContext(this ControllerBase controller)
+        {
+            var permission = (PermissionLevel?)controller.HttpContext.Items[nameof(PermissionLevel)];
+            return permission == null ? PermissionLevel.None : (PermissionLevel)permission;
         }
     }
 }
