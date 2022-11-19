7f51cd0da38972cafdf652e25c3abe82502f096e
5chmidt <pschmidt@buffalo.edu>
2022-11-19 11:14:18 -0500

    Added comments to permissions.

--- BookieWookie/BookieWookie.API/Authorization/PermissionLevel.cs
+++ BookieWookie/BookieWookie.API/Authorization/PermissionLevel.cs
@@ -1,12 +1,38 @@
 ﻿namespace BookieWookie.API.Authorization
 {
+    /// <summary>
+    /// Enumerated permissions in acending order of access.
+    /// </summary>
     public enum PermissionLevel
     {
+        /// <summary>
+        /// 0 - No permissions to execurte actions.
+        /// </summary>
         None,
+
+        /// <summary>
+        /// Read permission.
+        /// </summary>
         Get,
+
+        /// <summary>
+        /// Update permission.
+        /// </summary>
         Update,
+
+        /// <summary>
+        /// Create permission.
+        /// </summary>
         Create,
+
+        /// <summary>
+        /// Delete permission.
+        /// </summary>
         Delete,
+
+        /// <summary>
+        /// Highest level of permisison (grant to admin users only).
+        /// </summary>
         Admin
     }
 }
