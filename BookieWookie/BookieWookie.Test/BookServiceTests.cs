c87b67a3b4a672987ac94a9e30c92e8ec285ad48
5chmidt <pschmidt@buffalo.edu>
2022-11-19 22:25:17 -0500

    Added empty service tests.

---
+++ BookieWookie/BookieWookie.Test/BookServiceTests.cs
@@ -0,0 +1,26 @@
+﻿namespace BookieWookie.Test
+{
+    using NUnit.Framework;
+    using NUnit.Framework.Internal;
+
+    public class BookServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test()
+        {
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
